2d3bf6b8919ae67c32cd1a84b3d6eeccd4422eb1
wcampbell <wcampbell1995@gmail.com>
2022-08-20 16:08:33 +0000

    Working complete statusbar

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![feature(string_remove_matches)]
 
 use chrono::{DateTime, Local};
+use local_ip_address::list_afinet_netifas;
 use std::fmt::Write;
+use std::net::IpAddr;
 use std::sync::{mpsc::channel, Arc, Mutex};
 use std::time::Duration;
-use sysinfo::{CpuExt, NetworkExt, NetworksExt, ProcessExt, System, SystemExt, UserExt};
+use sysinfo::{CpuExt, System, SystemExt, UserExt};
 
 #[link(name = "X11")]
 extern "C" {
@@ -15,25 +17,41 @@ extern "C" {
 }
 
 fn main() {
+    let mut ip_addresses = vec![];
     let (bat0_tx, bat0_rx) = channel();
     let (bat1_tx, bat1_rx) = channel();
     let (mem_tx, mem_rx) = channel();
     let (cpu_tx, cpu_rx) = channel();
-    let mut sys_host_name = String::new();
-    let mut sys_user_name = String::new();
     let m_sys = Arc::new(Mutex::new(System::new_all()));
-    {
+
+    // First call to sys functions, grabbing host_name and user name, and also ip addresses
+    let (sys_host_name, sys_user_name) = {
         let mut sys = m_sys.lock().unwrap();
 
         sys.refresh_all();
 
-        sys_host_name = sys.host_name().unwrap().to_string();
-        sys_user_name = sys.users()[1].name().to_string();
+        let network_interfaces = list_afinet_netifas().unwrap();
+        for (_, ip) in network_interfaces.iter().filter(|(name, ip)| {
+            *name == "wlan0" || *name == "enp0s31f6" && matches!(ip, IpAddr::V4(_))
+        }) {
+            ip_addresses.push(ip.to_string());
+        }
 
-        for (interface, _) in sys.networks() {
-            println!("{}", interface);
+        (sys.host_name().unwrap(), sys.users()[1].name().to_string())
+    };
+
+    // create ip addresses string
+    let mut ip_addresses_string = "[".to_string();
+    for (i, address) in ip_addresses.iter().enumerate() {
+        ip_addresses_string += &address.to_string();
+
+        if i != ip_addresses.len() - 1 {
+            ip_addresses_string += ", ";
         }
     }
+    ip_addresses_string += "]";
+
+    // Thread updating every n seconds
     std::thread::scope(|x| {
         x.spawn(move || {
             loop {
@@ -54,10 +72,10 @@ fn main() {
                 let lines = &ram.split('\n').collect::<Vec<&str>>();
                 // Memory Total
                 let mem_total = lines[0].split_ascii_whitespace().collect::<Vec<&str>>();
-                let mem_total = u64::from_str_radix(mem_total[1], 10).unwrap();
+                let mem_total = mem_total[1].parse::<u64>().unwrap();
                 // Memory Free
                 let mem_free = lines[1].split_ascii_whitespace().collect::<Vec<&str>>();
-                let mem_free = u64::from_str_radix(mem_free[1], 10).unwrap();
+                let mem_free = mem_free[1].parse::<u64>().unwrap();
 
                 let memory_usage = mem_total / mem_free;
                 mem_tx.send(memory_usage).unwrap();
@@ -71,7 +89,7 @@ fn main() {
                         .ceil();
                 cpu_tx.send(new_avg_cpu_usage).unwrap();
 
-                std::thread::sleep(Duration::from_secs(2));
+                std::thread::sleep(Duration::from_secs(1));
             }
         });
 
@@ -101,14 +119,14 @@ fn main() {
                     last_bat1 = bat1.clone();
                 }
                 if let Ok(mem_usage) = mem_rx.try_recv() {
-                    last_mem_usage = mem_usage.clone();
+                    last_mem_usage = mem_usage;
                 }
                 if let Ok(cpu_usage) = cpu_rx.try_recv() {
-                    last_cpu_usage = cpu_usage.clone();
+                    last_cpu_usage = cpu_usage;
                 }
                 write!(
                     status,
-                    "[{sys_host_name}][{sys_user_name}] => cpu {last_cpu_usage}%, mem {last_mem_usage}%, bat [{last_bat0}%, {last_bat1}%], {}\0",
+                    "[{sys_host_name}][{sys_user_name}] => cpu {last_cpu_usage}%, mem {last_mem_usage}%, net {ip_addresses_string}, bat [{last_bat0}%, {last_bat1}%], {}\0",
                     local.format("%F %T")
                 )
                 .unwrap();
